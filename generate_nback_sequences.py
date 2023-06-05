6b9407508b6c6872011abd2795adf245816e9eeb
Dongyu Gong <41035051+Daniel-Gong@users.noreply.github.com>
2023-06-05 17:24:30 +0100

    Create generate_nback_sequences.py
    script

---
+++ generate_nback_sequences.py
@@ -0,0 +1,38 @@
+def generate_nback_sequences(n, alphabet, seq_length, matches, num_sequences):
+    sequences = []
+    for _ in range(num_sequences):
+        seq, conditions = [], []
+        match_positions = random.sample(range(n, seq_length), matches)
+        match_positions.sort()
+
+        for i in range(seq_length):
+            if i in match_positions:
+                seq.append(seq[i - n])
+                conditions.append('m')
+            else:
+                random_letter = random.choice(alphabet)
+                while i > n and seq[i - n] == random_letter:
+                    random_letter = random.choice(alphabet)
+                seq.append(random_letter)
+                conditions.append('-')
+        
+        sequences.append((''.join(seq), ''.join(conditions)))
+
+    return sequences
+
+def save_sequences_to_files(folder, sequences):
+    os.makedirs(folder, exist_ok=True)
+
+    for i, (seq, conditions) in enumerate(sequences):
+        with open(os.path.join(folder, f"{i}.txt"), "w") as file:
+            file.write(f"{seq}\n{conditions}\n")
+
+alphabet = 'bcdfghjklnpqrstvwxyz'
+seq_length = 30
+matches = 10
+num_sequences = 50
+
+for n in [1, 2, 3]:
+    sequences = generate_nback_sequences(n, alphabet, seq_length, matches, num_sequences)
+    folder = os.path.join("datasets", f"{n}back")
+    save_sequences_to_files(folder, sequences)
